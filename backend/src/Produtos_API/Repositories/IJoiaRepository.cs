4294b173f828b3a51bb5e80cb1ea333a214c4b28
Eduardo <eduardo.olvreis@gmail.com>
2026-04-21 17:44:48 -0300

    feat: repositories de joia implementados

---
+++ backend/src/Produtos_API/Repositories/IJoiaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Produtos_API.Models;
+
+namespace Produtos_API.Repositories
+{
+    public interface IJoiaRepository
+    {
+        Task<Joia?> ObterPorIdAsync(int id);
+        Task<IEnumerable<Joia>> ObterTodosAsync();
+        Task<Joia> CriarJoiaAsync(Joia joia);
+        Task<Joia> AtualizarJoiaAsync(Joia joia);
+        Task<bool> DeletarJoiaAsync(int id);
+    }
+}
---
+++ backend/src/Produtos_API/Repositories/JoiaRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Produtos_API.Data;
+using Produtos_API.Models;
+
+namespace Produtos_API.Repositories
+{
+    public class JoiaRepository : IJoiaRepository
+    {
+        private readonly AppDbContext _context;
+        public JoiaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Joia?> ObterPorIdAsync(int id)
+        {
+            var joia = await _context.Joias.AsNoTracking().FirstOrDefaultAsync(j => j.Id == id);
+            return joia;
+        }
+
+        public async Task<IEnumerable<Joia>> ObterTodosAsync()
+        {
+            var joias = await _context.Joias.ToListAsync();
+            return joias;
+        }
+
+        public async Task<Joia> CriarJoiaAsync(Joia joia)
+        {
+            _context.Joias.Add(joia);
+            await _context.SaveChangesAsync();
+            return joia;
+        }
+
+        public async Task<Joia> AtualizarJoiaAsync(Joia joia)
+        {
+            var encontrouJoia = await ObterPorIdAsync(joia.Id);
+            if (encontrouJoia == null) { return null; }
+            _context.Update(joia);
+            await _context.SaveChangesAsync();
+            return joia;
+        }
+
+        public async Task<bool> DeletarJoiaAsync(int id)
+        {
+            var encontrouJoia = await ObterPorIdAsync(id);
+            if(encontrouJoia == null) { return false; }
+            _context.Remove(encontrouJoia);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
