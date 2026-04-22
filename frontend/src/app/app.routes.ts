4b1f1a030b804b8246fda7e04eb2d8aa1b719c22
Eduardo <eduardo.olvreis@gmail.com>
2026-04-22 11:00:26 -0300

    feat: adição dos links da approutes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { JoiaListaComponent } from './components/joia-lista/joia-lista';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "produtos:/categoria", component: JoiaListaComponent }
+];
--- frontend/src/app/components/joia-lista/joia-lista.ts
+++ frontend/src/app/components/joia-lista/joia-lista.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './joia-lista.html',
   styleUrl: './joia-lista.scss',
 })
-export class JoiaLista {}
+export class JoiaListaComponent {}
