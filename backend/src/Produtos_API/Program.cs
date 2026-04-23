78d7a598458e1c8bb3d876df1898f949a39cf1ef
Eduardo <eduardo.olvreis@gmail.com>
2026-04-23 13:58:27 -0300

    feat: jwtbearer na program

--- backend/src/Produtos_API/Program.cs
+++ backend/src/Produtos_API/Program.cs
@@ -2,7 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using Produtos_API.Data;
 using Produtos_API.Repositories;
 using Scalar.AspNetCore;
+using System.Text;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
@@ -21,6 +24,27 @@ builder.Services.AddCors(options =>
         policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
+var secretKey = builder.Configuration["JwtSettings:Secret"];
+if (string.IsNullOrEmpty(secretKey))
+{
+    throw new InvalidOperationException("A Chave Secreta do JWT não foi configurada");
+}
+var key = Encoding.UTF8.GetBytes(secretKey);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -29,6 +53,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
