bf61fdf9594c55cc95453cdfcb2c6a6436131a55
Eduardo <eduardo.olvreis@gmail.com>
2026-04-23 17:56:51 -0300

    feat: adição do tailwind e inicio do header

---
+++ frontend/src/app/components/header/header.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink, CommonModule],
+  templateUrl: './header.html'
+})
+export class HeaderComponent {}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ouro': '#D4AF37',
+      }
+    },
+  },
+  plugins: [],
+}
