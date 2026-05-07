b8d15bb1127db8de61584b392850b111502e0605
Eduardo <eduardo.olvreis@gmail.com>
2026-05-07 17:41:35 -0300

    feat: blur ao abrir o menu lateral

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -9,7 +9,16 @@ import { RouterLink } from "@angular/router";
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  constructor(private renderer: Renderer2) {}
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    const body = document.body;
+    if (this.isMenuOpen) {
+      this.renderer.setStyle(body, 'overflow', 'hidden');
+      this.renderer.addClass(body, 'menu-is-open');
+    } else {
+      this.renderer.removeStyle(body, 'overflow');
+      this.renderer.removeClass(body, 'menu-is-open');
+    }
   }
 }
