2e8a16c65d6e6f2220e68124ce3385d89b3286d5
Eduardo <eduardo.olvreis@gmail.com>
2026-05-07 15:47:00 -0300

    feat: logos no header e estilização base

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, CommonModule],
+  imports: [CommonModule],
   templateUrl: './header.html'
 })
 export class HeaderComponent {}
