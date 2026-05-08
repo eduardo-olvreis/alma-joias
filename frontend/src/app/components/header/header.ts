4947e9fa47b523dee9192535e40627d62134823e
Eduardo <eduardo.olvreis@gmail.com>
2026-05-08 11:12:13 -0300

    fix: blur ao abrir o menu

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -7,18 +7,27 @@ import { RouterLink } from "@angular/router";
   imports: [CommonModule, RouterLink],
   templateUrl: './header.html'
 })
+
 export class HeaderComponent {
   isMenuOpen = false;
+  scrollbarWidth = 0;
   constructor(private renderer: Renderer2) {}
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
     const body = document.body;
+    const html = document.documentElement;
     if (this.isMenuOpen) {
+      this.scrollbarWidth = window.innerWidth - html.clientWidth;
+      this.renderer.setStyle(html, 'overflow', 'hidden');
       this.renderer.setStyle(body, 'overflow', 'hidden');
+      this.renderer.setStyle(body, 'padding-right', `${this.scrollbarWidth}px`);
       this.renderer.addClass(body, 'menu-is-open');
     } else {
+      this.renderer.removeStyle(html, 'overflow');
       this.renderer.removeStyle(body, 'overflow');
+      this.renderer.removeStyle(body, 'padding-right');
       this.renderer.removeClass(body, 'menu-is-open');
+      this.scrollbarWidth = 0;
     }
   }
 }
