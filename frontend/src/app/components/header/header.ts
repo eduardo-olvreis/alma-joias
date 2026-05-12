45fe04b72628e0b0c9e8f58644d8aa7e088fa722
Eduardo <eduardo.olvreis@gmail.com>
2026-05-12 17:04:32 -0300

    feat: implementação do hero (provisório)

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,23 +1,45 @@
+import { Component, Renderer2, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, Renderer2 } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './header.html'
 })
-
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit, OnDestroy {
   isMenuOpen = false;
   scrollbarWidth = 0;
-  constructor(private renderer: Renderer2) {}
+  private observer: ResizeObserver | undefined;
+
+  constructor(
+    private renderer: Renderer2, 
+    private el: ElementRef
+  ) {}
+
+  ngAfterViewInit() {
+    this.observer = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        const height = entry.target.clientHeight;
+        document.documentElement.style.setProperty('--header-height', `${height}px`);
+      }
+    });
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.observer?.disconnect();
+  }
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
     const body = document.body;
     const html = document.documentElement;
+
     if (this.isMenuOpen) {
       this.scrollbarWidth = window.innerWidth - html.clientWidth;
+      
       this.renderer.setStyle(html, 'overflow', 'hidden');
       this.renderer.setStyle(body, 'overflow', 'hidden');
       this.renderer.setStyle(body, 'padding-right', `${this.scrollbarWidth}px`);
