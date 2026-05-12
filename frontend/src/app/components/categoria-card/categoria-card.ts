08ea4c851c42bc39e69a0579c79f0dda27dd5b88
Eduardo <eduardo.olvreis@gmail.com>
2026-05-12 16:15:32 -0300

    feat: card component adicionado na home

--- frontend/src/app/components/categoria-card/categoria-card.ts
+++ frontend/src/app/components/categoria-card/categoria-card.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categoria-card',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './categoria-card.html',
   styles: ``,
   template: `
--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -9,4 +9,11 @@ import { CategoriaCard } from '../categoria-card/categoria-card';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  categorias = [
+    { nome: 'Anéis', link: '/produtos/Anel', imagem: '/images/categorias/anel_categoria.png' },
+    { nome: 'Brincos', link: '/produtos/Brinco', imagem: '/images/categorias/brinco_categoria.png' },
+    { nome: 'Colares', link: '/produtos/Colar', imagem: '/images/categorias/colar_categoria.png' },
+    { nome: 'Pulseiras', link: '/produtos/Pulseira', imagem: '/images/categorias/pulseira_categoria.png' }
+  ]
+}
