07a163a8ee62dbda97036343f2b58b7f81f4f5b4
Eduardo <eduardo.olvreis@gmail.com>
2026-05-21 17:33:10 -0300

    feat: texto no hero

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       colors: {
         'ouro': '#D4AF37',
+        'primary-text-color': 'var(--primary-text-color)',
+        'secondary-text-color': 'var(--secondary-text-color)',
+        'highlight-color': 'var(--highlight-text-color)'
       }
     },
   },
